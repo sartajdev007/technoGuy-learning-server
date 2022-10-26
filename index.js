c29c2853e7fbf7323a304480d975359245ac4c86
sartajdev007 <sartaj5akik@gmail.com>
2022-10-27 01:32:02 +0600

    course data send from server

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000
+
+const categories = require('./data/categories.json');
+const { request } = require('express');
+const courses = require('./data/courses.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Api running')
+})
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.listen(port, () => {
+    console.log('server running', port)
+})
